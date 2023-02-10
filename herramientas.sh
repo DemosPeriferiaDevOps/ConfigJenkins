8dbec8ca41178153d918c1145864db3be842e33c
demoperiferia <101884714+DemosPeriferiaDevOps@users.noreply.github.com>
2023-02-10 10:49:45 -0500

    Update herramientas.sh

--- herramientas.sh
+++ herramientas.sh
@@ -41,5 +41,3 @@ echo "TERRAFORM"
 sudo wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform
-echo "CLONAR REPO"
-sudo -u ubuntu sh -c "cd /home/ubuntu; git clone https://github.com/DemosPeriferiaDevOps/ConfigJenkins.git; cd ConfigJenkins; ansible-playbook jenkins.yaml"
